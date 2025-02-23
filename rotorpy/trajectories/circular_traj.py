22f9f109d9a1e903afda0df814d158f74e8c6283
spencerfolk <sfolk@seas.upenn.edu>
2025-02-23 18:48:25 -0500

    Added batched versions of circular traj and hover traj.

--- rotorpy/trajectories/circular_traj.py
+++ rotorpy/trajectories/circular_traj.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 import sys
 
 class CircularTraj(object):
@@ -116,9 +117,9 @@ class CircularTraj(object):
             yaw_ddot = -np.pi*np.pi*np.pi/4*np.sin(np.pi*t)
 
         else:
-            yaw = 0
-            yaw_dot = 0
-            yaw_ddot = 0
+            yaw = 0.0
+            yaw_dot = 0.0
+            yaw_ddot = 0.0
 
         flat_output = { 'x':x, 'x_dot':x_dot, 'x_ddot':x_ddot, 'x_dddot':x_dddot, 'x_ddddot':x_ddddot,
                         'yaw':yaw, 'yaw_dot':yaw_dot, 'yaw_ddot':yaw_ddot}
@@ -183,10 +184,69 @@ class ThreeDCircularTraj(object):
         if self.yaw_bool:
             yaw = 0.8*np.pi/2*np.sin(2.5*t)
             yaw_dot = 0.8*2.5*np.pi/2*np.cos(2.5*t)
+            yaw_ddot = 0.8*(2.5**2)*np.pi/2*np.sin(2.5*t)
         else:
-            yaw = 0
-            yaw_dot = 0
+            yaw = 0.0
+            yaw_dot = 0.0
+            yaw_ddot = 0.0
 
         flat_output = { 'x':x, 'x_dot':x_dot, 'x_ddot':x_ddot, 'x_dddot':x_dddot, 'x_ddddot':x_ddddot,
-                        'yaw':yaw, 'yaw_dot':yaw_dot}
+                        'yaw':yaw, 'yaw_dot':yaw_dot, 'yaw_ddot':yaw_ddot}
+        return flat_output
+
+class BatchedThreeDCircularTraj(object):
+    """ 
+    A batched version of the ThreeDCircularTraj.
+    """
+
+    def __init__(self, centers, radii, freqs, yaw_bools, device='cpu'):
+        """ 
+        This is the batched version of ThreeDCircularTraj. Each input must be a list of the same length, M, 
+        where M is the number of circles to be completed.
+        Inputs:
+            centers, the center of each circle (m)
+            radii, the radius of each circle (m)
+            freqs, the frequency with which each circle is completed (Hz)
+        """
+
+        assert len(centers) == len(radii) == len(freqs) == len(yaw_bools), "BatchedThreeDCircularTraj Error: inputs must be of the same length"
+        self.M = len(centers)
+
+        self.centers = torch.tensor(centers, device=device)
+        self.radii = torch.tensor(radii, device=device)
+        self.freqs = torch.tensor(freqs)
+        self.yaw_bools = torch.tensor(yaw_bools, device=device)
+
+        self.omegas = (2*np.pi*self.freqs).to(device)
+
+        return 
+
+    def update(self, t):
+        """ 
+        Given the present time, return the desired flat output and derivatives.
+        """ 
+
+        x = torch.stack([self.centers[:,0] + self.radii[:,0]*torch.cos(self.omegas[:,0]*t),
+                            self.centers[:,1] + self.radii[:,1]*torch.sin(self.omegas[:,1]*t),
+                            self.centers[:,2] + self.radii[:,2]*torch.sin(self.omegas[:,2]*t)], dim=1)
+        x_dot = torch.stack([-self.radii[:,0]*self.omegas[:,0]*torch.sin(self.omegas[:,0]*t),
+                            self.radii[:,1]*self.omegas[:,1]*torch.cos(self.omegas[:,1]*t),
+                            self.radii[:,2]*self.omegas[:,2]*torch.cos(self.omegas[:,2]*t)], dim=1)
+        x_ddot = torch.stack([-self.radii[:,0]*(self.omegas[:,0]**2)*torch.cos(self.omegas[:,0]*t),
+                            -self.radii[:,1]*(self.omegas[:,1]**2)*torch.sin(self.omegas[:,1]*t),
+                            -self.radii[:,2]*(self.omegas[:,2]**2)*torch.sin(self.omegas[:,2]*t)], dim=1)
+        x_dddot = torch.stack([ self.radii[:,0]*(self.omegas[:,0]**3)*torch.sin(self.omegas[:,0]*t),
+                            -self.radii[:,1]*(self.omegas[:,1]**3)*torch.cos(self.omegas[:,1]*t),
+                             self.radii[:,2]*(self.omegas[:,2]**3)*torch.cos(self.omegas[:,2]*t)], dim=1)
+        x_ddddot = torch.stack([self.radii[:,0]*(self.omegas[:,0]**4)*torch.cos(self.omegas[:,0]*t),
+                            self.radii[:,1]*(self.omegas[:,1]**4)*torch.sin(self.omegas[:,1]*t),
+                            self.radii[:,2]*(self.omegas[:,2]**4)*torch.sin(self.omegas[:,2]*t)], dim=1)
+                        
+        yaw = 0.8*np.pi/2*np.sin(2.5*t)*self.yaw_bools
+        yaw_dot = 0.8*2.5*np.pi/2*np.cos(2.5*t)*self.yaw_bools
+        yaw_ddot = 0.8*(2.5**2)*np.pi/2*np.sin(2.5*t)*self.yaw_bools
+
+        flat_output = { 'x':x, 'x_dot':x_dot, 'x_ddot':x_ddot, 'x_dddot':x_dddot, 'x_ddddot':x_ddddot,
+                        'yaw':yaw, 'yaw_dot':yaw_dot, 'yaw_ddot':yaw_ddot}
+
         return flat_output
