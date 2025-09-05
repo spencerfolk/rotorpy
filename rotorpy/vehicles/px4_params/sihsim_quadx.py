867ea60c0e9d2fbd3f3f5726320ab8fb6f084468
Davide Iafrate <dvde.iafrate98@gmail.com>
2025-09-05 18:54:49 +0200

    Working parameters for px4 quadx airframe

--- rotorpy/vehicles/px4_params/sihsim_quadx.py
+++ rotorpy/vehicles/px4_params/sihsim_quadx.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+d = 0.17 # distance from CoM to rotor (m)
+
 # 10040_sihsim_quadx preset (aligned with PX4 SIH parameters)
 sihsim_quadx = {
     'mass':             1.0,     # kg      (PX4 param SIH_MASS)
@@ -12,22 +14,22 @@ sihsim_quadx = {
 
     'num_rotors':       4,
     'rotor_pos': {
-        'r1':           np.array([ 1.0,  1.0, 0.0]),
-        'r2':           np.array([-1.0, -1.0, 0.0]),
-        'r3':           np.array([ 1.0, -1.0, 0.0]),
-        'r4':           np.array([-1.0,  1.0, 0.0]),
+        'r1':           d*np.array([ 1.0,  -1.0, 0.0]),
+        'r2':           d*np.array([-1.0,   1.0, 0.0]),
+        'r3':           d*np.array([ 1.0,   1.0, 0.0]),
+        'r4':           d*np.array([-1.0,  -1.0, 0.0]),
     },
 
-    # Sign for each motor’s yaw moment
-    'rotor_directions': np.array([ 1, 1, -1, -1 ]),
+    # Sign for each motor’s yaw moment (+ CW, - CCW)
+    'rotor_directions': np.array([ -1, -1, 1, 1 ]),
     'rI':               np.array([0.0, 0.0, 0.0]),
 
     'c_Dx':             1.0,
     'c_Dy':             1.0,
     'c_Dz':             1.0,
 
-    'k_eta':            1.0,    # max thrust per rotor (N) → SIH_T_MAX
-    'k_m':              0.1,    # max yaw moment per rotor (Nm) → SIH_Q_MAX
+    'k_eta':            5e-6,    # This is computed as SIH_T_MAX/(rotor_speed_max^2)
+    'k_m':              1e-7,    # max yaw moment per rotor (Nm) → SIH_Q_MAX/(rotor_speed_max^2)
     'k_d':              0.0,    # rotor drag
     'k_z':              0.0,    # induced inflow
     'k_h':              0.0,    # translational lift
@@ -35,7 +37,7 @@ sihsim_quadx = {
 
     'tau_m':            0.05,   # Motor response time constant (s) ← SIH_T_TAU
     'rotor_speed_min':  0.0,    # [rad/s] zero throttle
-    'rotor_speed_max':  838.0,  # [rad/s] full throttle
+    'rotor_speed_max':  1000.0,  # [rad/s] full throttle
     'motor_noise_std':  0.0,
 }
 
